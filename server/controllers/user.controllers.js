a59a2c813cbd2f0e6fc1aa5c6a6407e1849b0c30
Augustin Tacquet <augu.tacquet@gmail.com>
2022-01-15 11:31:24 +0100

    login works in backend without auth middleware

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -19,6 +19,30 @@ exports.signup = (req, res, next) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
+exports.login = (req, res, next) => {
+  User.findOne({ where: { email: req.body.email } })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          res.status(200).json({
+            userId: user.id,
+            token: jwt.sign({ userId: user.id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
 exports.getOneUser = (req, res, next) => {
   const id = req.params.id;
   User.findOne({ where: { id: id } })
